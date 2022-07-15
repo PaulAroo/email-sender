7ffa5c16f1ca461ccf022a489bf5cb1dd32c1df0
Paul_Aroo <timmypaul2006@gmail.com>
2022-07-15 22:14:20 +0100

    implement: mail transport

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const express = require("express");
+const mailer = require("nodemailer");
 
 require("dotenv").config();
 
-const { PORT } = process.env;
+const {
+  PORT,
+  MAIL_USERNAME,
+  MAIL_PASSWORD,
+  OAUTH_CLIENTID,
+  OAUTH_CLIENT_SECRET,
+  OAUTH_REFRESH_TOKEN,
+} = process.env;
 
 const app = express();
+
+const emailSender = mailer.createTransport({
+  service: "gmail",
+  auth: {
+    type: "OAuth2",
+    user: MAIL_USERNAME,
+    pass: MAIL_PASSWORD,
+    clientId: OAUTH_CLIENTID,
+    clientSecret: OAUTH_CLIENT_SECRET,
+    refreshToken: OAUTH_REFRESH_TOKEN,
+  },
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World");

60da97416e64e37fb1a154ae3488e03c87603afe
Paul_Aroo <timmypaul2006@gmail.com>
2022-07-15 23:25:07 +0100

    implement: send automated email

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { json } = require("express");
 const mailer = require("nodemailer");
 
 require("dotenv").config();
@@ -14,6 +15,8 @@ const {
 
 const app = express();
 
+app.use(json());
+
 const emailSender = mailer.createTransport({
   service: "gmail",
   auth: {
@@ -26,8 +29,40 @@ const emailSender = mailer.createTransport({
   },
 });
 
+function sendMail(emailAddress, res) {
+  emailSender.sendMail(
+    {
+      from: "timmypaul2006@gmail.com",
+      to: `timmypaul2006@gmail.com, ${emailAddress}`,
+      subject: "sending Automated Emails",
+      text: "tests sending Automated Emails using nodemailer",
+      html: `<h1>Hello Friend 🙌, I hope this meets you well This is a test to see if sending automated emails works on this application, a copy of this mail will be sent to both ${emailAddress} and ${MAIL_USERNAME}</h1>`,
+    },
+    (err, info) => {
+      if (err) {
+        console.log(err);
+        return res.status(400).json({ error: "failed to send email" });
+      } else {
+        res.json({ message: "email successfully sent", data: info });
+      }
+    }
+  );
+}
+
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.json({
+    message:
+      "use /sendMail/<emailAddress> to send an automated email to the specified email address",
+  });
+});
+
+app.get("/sendMail/:emailAddress", (req, res) => {
+  const { emailAddress } = req.params;
+  if (emailAddress) {
+    sendMail(emailAddress, res);
+  } else {
+    res.send("please provide an email address");
+  }
 });
 
 app.listen(PORT, (err) => {
